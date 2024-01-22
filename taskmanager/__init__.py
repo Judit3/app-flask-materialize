8a9624f732c261086e9511faefa531f1a0bf1867
Judit3 3 <katapunpinpun@yahoo.es>
2024-01-22 16:20:53 +0000

    add models.py to templates

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,14 +1,15 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-if os.path.exist("env.py"):
-    import env # noqa
+if os.path.exists("env.py"):
+    import env  # noqa
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLACHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
-from taskmanager import  # noqa
+from taskmanager import routes  # noqa
     
